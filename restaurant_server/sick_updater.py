327f2721cdb648ef45ba256e06582065f0335b6b
Mark Kettner <mark@kettner.io>
2020-06-28 19:34:17 +0200

    einfacherer zugang für beamten_backend (see Issue #1)

--- restaurant_server/sick_updater.py
+++ restaurant_server/sick_updater.py
@@ -1,33 +1,20 @@
 #!/usr/bin/env python3
 
-CORONA_FILE="/var/www/html/corona.txt"
-#CORONA_FILE="./corona.txt"
+#CORONA_FILE="/var/www/html/corona.txt"
+CORONA_FILE="./corona.txt"
 date_fmt="%d-%m-%y-%H:%M"
 
 from datetime import datetime
+from sys import stdin
 
-print("Hello Beamter, what do you want?")
-print("(0) append data to corona file")
-print("(1) ping")
+print("Enter new stuff:")
 
-def append():
-    # TODO check if start is a valid UTC timestamp
-    start = input(f"enter start date+time ({date_fmt}):")
-    try:
-        start = datetime.strptime(start, date_fmt)
-    except Exception:
-        print("wrong format")
-    duration = int(input("enter duration in minutes:"))
-    with open(CORONA_FILE, "a") as fd:
+with open(CORONA_FILE, "a") as fd:
+    for l in stdin.readlines():
+        try:
+            start, duration = l.split()
+            start = datetime.strptime(start, date_fmt)
+            duration = int(duration)
+        except Exception:
+            continue
         print(start.strftime(date_fmt), duration, file=fd)
-    print("current state:")
-    with open(CORONA_FILE, "r") as fd:
-        print(*fd.readlines(), sep='', end='')
-    print("thank you!")
-
-def pong():
-    print("pong")
-
-{'0': append,
- '1': pong
-}.get(input(), pong)()
